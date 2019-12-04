5856653bae76c7f534a030773ed26880221a2b42
Ian Egner <ian@egnerphotography.co.uk>
2019-12-04 19:06:14 +0000

    Revert "init simple router functionality"
    This reverts commit 85cc4618513ad4c22dca2df1e0f227770863809f.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from './Router'
+import App from './App';
 
 ReactDOM.render(
-  <Router />,
+  <App />,
   document.getElementById('root')
 );
